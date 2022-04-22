95745ce963d526e4ff0707bd1576ae0d092692e1
Wsko-12 <vaskovladislav@gmail.com>
2022-04-22 12:53:04 +0300

    feat: add keyboard layout arr

--- js/classes/Keyboard.js
+++ js/classes/Keyboard.js
@@ -4,7 +4,14 @@ import Layout from '../layout/layout.js';
 
 export default class Keyboard {
   constructor() {
-    this.output = Layout.createEl('textarea', { classes: ['output'], attrs: { rows: 5, cols: 20 } });
+    this.layout = [
+      ['Backquote', 'Digit1', 'Digit2', 'Digit3', 'Digit4', 'Digit5', 'Digit6', 'Digit7', 'Digit8', 'Digit9', 'Digit0', 'Minus', 'Equal', 'Backspace'],
+      ['Tab', 'KeyQ', 'KeyW', 'KeyE', 'KeyR', 'KeyT', 'KeyY', 'KeyY', 'KeyU', 'KeyI', 'KeyO', 'KeyP', 'BracketLeft', 'BracketRight', 'Backslash'],
+      ['CapsLock', 'KeyA', 'KeyS', 'KeyD', 'KeyF', 'KeyG', 'KeyH', 'KeyJ', 'KeyK', 'KeyL', 'Semicolon', 'Quote', 'Enter'],
+      ['ShiftLeft', 'KeyZ', 'KeyX', 'KeyC', 'KeyV', 'KeyB', 'KeyN', 'KeyM', 'Comma', 'Period', 'Slash', 'ArrowUp', 'ShiftRight'],
+      ['ControlLeft', 'MetaLeft', 'AltLeft', 'Space', 'AltRight', 'ControlRight', 'ArrowLeft', 'ArrowLeft', 'ArrowDown', 'ArrowRight'],
+    ];
+    this.output = Layout.createEl('textarea', { classes: ['output'], attrs: { rows: 8, cols: 64 } });
     this.container = Layout.createEl('div', { classes: ['keyboard'] });
 
     this.wrapper = Layout.createEl('div', { classes: ['wrapper'], content: [this.output, this.container] });
